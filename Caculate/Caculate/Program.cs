25e42c42cf908f75687d4ef7a0d1075a24276156
devnam1401 <namphhe180085@fpt.edu.vn>
2026-01-29 09:35:01 +0700

    Fix missing plus case after rebase

--- Caculate/Caculate/Program.cs
+++ Caculate/Caculate/Program.cs
@@ -18,6 +18,8 @@
         {
             switch (sign)
             {
+                case SIGN.PLUS:
+                    return a + b;
                 case SIGN.MINUS:
                     return a - b;
                 default:
